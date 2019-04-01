0a98e94865a311512d61e4d4ed03b264c3eb6215
TG-Techie <TGTechie01@gmail.com>
2019-04-01 16:45:45 -0400

    updates

--- artsy.py
+++ artsy.py
@@ -23,32 +23,34 @@ from tg_gui.gui import widget, round_rect, default_color, white, good_gap_size
 
 class canvas(widget):
 
-    def __init__(self, *args, border = good_gap_size, border_color = default_color, color = white):
-        super().__init__(*args)
-        print(*args)
+    def __init__(self, x, y, width, height, *args, border = good_gap_size, border_color = default_color, color = white, on_place = gui._do_nothing):
+        super().__init__(x+border, y+border, width-border*2, height-border*2, *args)
+        #print(*args)
         self._border = border
         self._border_color = border_color
         self._color = color
+        self.on_place = on_place
 
     def place(self):
         super().place()
-        print(self.width)
-        round_rect( self.physical_x,
-                    self.physical_y,
-                    self.width, self.height, 0,
-                    self._border_color)
+        #print(self.width)
+        round_rect( self.physical_x - self._border,
+                    self.physical_y - self._border,
+                    self.width + self._border*2, self.height + self._border*2,
+                    0, self._border_color)
         self.clear()
+        self.on_place(self)
 
 
     def draw_pixel(self, x, y, color):
-        if 0<x<self.width-self._border*2 and 0<y<self.height-self._border*2:
-            round_rect( self.physical_x + self._border + x,
-                        self.physical_y + self.height -self._border -y,
+        if 0<x<self.width and 0<y<self.height:
+            round_rect( self.physical_x + x,
+                        self.physical_y + self.height  -y,#-self._border
                         1, 1, 0, color)
 
     def clear(self):
-        round_rect( self.physical_x + self._border,
-                    self.physical_y + self._border,
-                    self.width - self._border*2,
-                    self.height - self._border*2,
+        round_rect( self.physical_x,
+                    self.physical_y,
+                    self.width,
+                    self.height,
                     0, self._color)
